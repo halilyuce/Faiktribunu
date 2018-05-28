e335d372a7c214af235f493d6fca6153bf69a041
supreme <supreme@muhammet-mac-mini.local>
2018-05-28 10:56:51 +0300

    Initial Commit

---
+++ faiktribunu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  faiktribunu
+//
+//  Created by Halil İbrahim YÜCE on 28.05.2018.
+//  Copyright © 2018 Halil İbrahim YÜCE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
